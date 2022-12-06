ec4f5780ca43d72cbc60296b5a64e9723c386d2e
saifndub <saif.ndub@gmail.com>
2022-12-06 10:42:13 +0600

    print welcome page

--- Django_Project/urls.py
+++ Django_Project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from MachineLearning import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.machine),
 ]
--- MachineLearning/views.py
+++ MachineLearning/views.py
@@ -2,3 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def machine(request):
+    return HttpResponse('Welcome to Django. Md Saif Hossain.')
