c3b1a8a1495047c0e4d8a00a0c3dc5fe079a41d8
saifndub <saif.ndub@gmail.com>
2023-01-07 12:12:10 +0600

    creating vertual environment

--- MachineLearning/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def machine(request):
-    return HttpResponse('Welcome to Django. Md Saif Hossain.')
